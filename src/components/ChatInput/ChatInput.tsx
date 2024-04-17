a9344d866c6fe0f0e51f9fa55b4b09b9cb9fa60d
heygwangjin <heygwangjin@gmail.com>
2024-04-18 01:37:19 +0900

    feat: Add optimistic message

--- src/components/ChatInput/ChatInput.tsx
+++ src/components/ChatInput/ChatInput.tsx
@@ -1,14 +1,39 @@
 "use client";
 
 import { Input } from "@/components/ui/input";
+import { Imessage, useMessage } from '@/lib/store/messages';
+import { useUser } from '@/lib/store/user';
 import { createClient } from "@/lib/supabase/client";
 import { toast } from "sonner";
+import { v4 as uuidv4 } from "uuid";
 
 function ChatInput() {
+  const user  = useUser((state) => state.user);
+  const addMessage = useMessage((state) => state.addMessage);
+
   const supabase = createClient();
 
   const handleSendMessage = async (text: string) => {
-    if (!text) return;
+    if (!text.trim()) {
+      toast.error("Message cannot be empty");
+      return;
+    }
+
+    const nextMessage = {
+      id: uuidv4(),
+      text,
+      user_id: user?.id,
+      is_edit: false,
+      created_at: new Date().toISOString(),
+      user: {
+        id: user?.id,
+        name: user?.user_metadata.name,
+        avatar_url: user?.user_metadata.avatar_url,
+        created_at: new Date().toISOString(),
+      }
+    }
+
+    addMessage(nextMessage as Imessage)
 
     const { error } = await supabase.from("message").insert({ text });
 
